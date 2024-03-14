aa8600754b8dd4f6c791afabd670d0b77fbc2ff5
AfrizalAlka <afrizal.alkautsar@gmail.com>
2024-03-14 10:41:38 +0700

    update ui splashscreen

--- lib/view/splashscreen.dart
+++ lib/view/splashscreen.dart
@@ -13,7 +13,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(const Duration(seconds: 4), () {
+    Timer(const Duration(seconds: 5), () {
       Navigator.of(context)
           .pushReplacement(MaterialPageRoute(builder: (_) => LoginUser()));
     });
@@ -22,21 +22,26 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 65, 65, 65),
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
       body: Center(
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Image.asset(
-            'images/app_logo.png',
+            'lib/images/app_logo.png',
             height: 200,
           ),
           const SizedBox(
             height: 20,
           ),
-          const CircularProgressIndicator.adaptive(
-            valueColor: AlwaysStoppedAnimation<Color>(Colors.grey),
-          )
+          Padding(
+            padding: const EdgeInsets.only(top: 230.0), // Atur jarak dari atas
+            child: CircularProgressIndicator.adaptive(
+              valueColor: AlwaysStoppedAnimation<Color>(
+                Color.fromARGB(255, 95, 95, 95),
+              ),
+            ),
+          ),
         ],
       )),
     );
